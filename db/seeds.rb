6923d9c75fc60eeaab7357cdd220fab2d7959758
gabrielle simon <gabriellesimon23@gmail.com>
2019-06-18 14:34:38 +0300

    yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'japanese',
+    phone_number: '0578392746'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'italian',
+    phone_number: '06298361443'
+  },
+  {
+    name:         'Baguette',
+    address:      'Champs Elysee 50',
+    category:     'french',
+    phone_number: '00335672132'
+  },
+  {
+    name:         'Pizza Hut',
+    address:      'Munich',
+    category:     'italian',
+    phone_number: '0067824156'
+  },
+  {
+    name:         'Waffles',
+    address:      'Brussels',
+    category:     'belgian',
+    phone_number: '004987162345'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
